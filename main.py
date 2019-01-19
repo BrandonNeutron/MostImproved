26e1f69bc04299d1666e6d893a06aaf485371684
Brandon Hahn <blh16b@my.fsu.edu>
2019-01-19 15:41:10 -0500

    Gets Basketball Reference URL of each player, stores in dict

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import urllib.request
 
+
+def findLastSpace(name):
+    firstSpace = name.find(" ")
+    while name.find(" ", firstSpace + 1) != -1:
+        firstSpace = name.find(" ", firstSpace + 1)
+
+    return firstSpace
+
+
+def getFirstName(name):
+    name = name[:findLastSpace(name)]
+    while name.find(".") != -1:
+        name = name[:name.find(".")] + name[name.find(".") + 1:]
+    while name.find("'") != -1:
+        name = name[:name.find("'")] + name[name.find("'") + 1:]
+    while name.find(" ") != -1:
+        name = name[:name.find(" ")] + name[name.find(" ") + 1:]
+    return name
+
 wikipedia = str(urllib.request.urlopen("https://en.wikipedia.org/wiki/NBA_Most_Improved_Player_Award").read())
 tableBegin = wikipedia.find("86 NBA season\">")
 MIPDict = {}
@@ -13,6 +32,8 @@ while i < 33:
         playerName = "Goran Dragic"
     elif playerName.__contains__("xbcrko"):
         playerName = "Hedo Turkoglu"
+    elif playerName.__contains__("Gheorghe"):
+        playerName = "Gheorghe Muresan"
     seasonWon += 1
     if seasonWon == 100:
         seasonWon = 0
@@ -20,7 +41,26 @@ while i < 33:
     tableBegin = postPlayer + 20
     i += 1
 
-print(MIPDict)
+for player in MIPDict:
+    if MIPDict[player] < 10:
+        MIPDict[player] = "0" + str(MIPDict[player])
+    else:
+        MIPDict[player] = str(MIPDict[player])
+
+bbref = "https://www.basketball-reference.com"
+URLEnd = ""
+URLDict = {}
+for player in MIPDict:
+    URLEnd = "/players/" + player[findLastSpace(player)+1] + "/" + player[findLastSpace(player) + 1:findLastSpace(player) + 6] + getFirstName(player)[:2] + "01.html"
+#    playerPage = str(urllib.request.urlopen(bbref + URLEnd).read())
+#    preURLEnd = playerPage.find("/players",playerPage.find(player) - 50)
+#    postURLEnd = playerPage.find(".html", preURLEnd) + 5
+#    URLEnd = playerPage[preURLEnd:postURLEnd]
+    URLEnd = URLEnd.lower()
+    URLDict[player] = bbref + URLEnd
+
+
+print(URLDict)
 
 
 
